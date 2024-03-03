5ef63eac2044b4b63bcc57d4e419c0d7365d46a6
Kshitij Ghimire <kshitijnep13@gmail.com>
2024-03-03 07:05:57 +0545

    work on editproduct

--- admin/actioncategory.php
+++ admin/actioncategory.php
@@ -1,65 +1,58 @@
 <?php
-if (isset($_POST['store'])) {
+session_start();
+if(isset($_POST['store']))
+{
     $name = $_POST['name'];
     $priority = $_POST['priority'];
 
-    $qry = "INSERT INTO categories(name,priority) VALUES ('$name','$priority')";
+    $qry = "INSERT INTO categories(name, priority) VALUES('$name', $priority)";
 
     include '../includes/dbconnection.php';
     $result = mysqli_query($conn, $qry);
-    if ($result) {
-        echo "<script>
-    alert('Category added successfully');
-    window.location.href='categories.php';
-    </script> ";
-    } else {
-        echo "error inserting data";
+    if($result){
+        $_SESSION['msg'] = "Category created successfully";
+        header('location: categories.php');
+    }else{
+        echo "Error adding category";
     }
-
     include '../includes/closeconnection.php';
 }
 
-
-// update
-if (isset($_POST['update'])) {
+//update
+if(isset($_POST['update']))
+{
     $id = $_POST['categoryid'];
     $name = $_POST['name'];
     $priority = $_POST['priority'];
 
-    $qry = "UPDATE categories SET name='$name',priority=$priority WHERE id=$id";
+    $qry = "UPDATE categories SET name = '$name', priority = $priority WHERE id = $id";
 
     include '../includes/dbconnection.php';
     $result = mysqli_query($conn, $qry);
     include '../includes/closeconnection.php';
 
-    if ($result) {
-        echo "<script>
-    alert('Category updated successfully');
-    window.location.href='categories.php';
-    </script> ";
-    } else {
-        echo "error updating category";
+    if($result){
+        $_SESSION['msg'] = "Category updated successfully";
+        header('location: categories.php');
+    }else{
+        echo "Error updating category";
     }
 }
 
-// Delete
-if (isset($_GET['deleteid'])) 
+//delete
+if(isset($_GET['deleteid']))
 {
-$id=$_GET['deleteid'];
-$qry="DELETE FROM categories WHERE id=$id";
+    $id = $_GET['deleteid'];
+    $qry = "DELETE FROM categories WHERE id = $id";
 
-include '../includes/dbconnection.php';
+    include '../includes/dbconnection.php';
     $result = mysqli_query($conn, $qry);
     include '../includes/closeconnection.php';
 
-    if ($result) {
-        echo "<script>
-    alert('Delete successfully');
-    window.location.href='categories.php';
-    </script> ";
-    } else {
-        echo "error delete category";
+    if($result){
+        $_SESSION['msg'] = "Category deleted successfully";
+        header('location: categories.php');
+    }else{
+        echo "Error deleting category";
     }
 }
-
-
